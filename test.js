5a85b9a91a6a92aaffc2f9f5e6ebe9d81f8abc5c
sahik7 <sahikahsan7@gmail.com>
2024-01-11 15:03:34 +0600

    Another example set for currying function

--- test.js
+++ test.js
@@ -12,4 +12,24 @@ const add = (a) => {
 console.log(add(1)(2)(3)) // 6
 
 
+// Another Example
+
+const level = add(5);
+console.log(level(2)(2));  // 9
+console.log(level(12)(4)); // 21
+console.log(level(54)(66)); // 125
+console.log(level(15)(12)); // 32
+console.log(level(2)(4));// 11
+
+6
+9
+21
+125
+32
+11
+
+
+
+
+
 
